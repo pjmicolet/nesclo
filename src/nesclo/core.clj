708f821b6b82b0977ac56bf8af3b90640197627b
pjm <p.micolet@gmail.com>
2015-04-14 11:39:21 +0100

    Updated Core to try disassembler.

--- src/nesclo/core.clj
+++ src/nesclo/core.clj
@@ -4,8 +4,22 @@
 (require 'nesclo.cpu)
 (refer 'nesclo.cpu)
 
+(require 'nesclo.apu)
+(refer 'nesclo.apu)
+
+(require 'nesclo.ppu)
+(refer 'nesclo.ppu)
+
+(require 'nesclo.rom)
+(refer 'nesclo.rom)
+
+(def filepath "C:/Users/pjm/Downloads/mario/supermario.nes")
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (hello))
+  (hello) 
+  (ppu_start) 
+  (helloPU)
+  (let[buffer (romread filepath)] (disassemble buffer 0)))
 
