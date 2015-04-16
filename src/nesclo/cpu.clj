2f8bd0d3ba28579d884f044b2b9f6ae097f74064
pjm <p.micolet@gmail.com>
2015-04-16 01:44:59 +0100

    Disassembly more work
    So I read more about instructions to wrap my head around it and changed the
    sizes because some of it didn't make sense (implied addressing should only be
    of size one). Still nowhere near sure whether or not this generates the correct result
    but we should be getting there.
    
    Also added addressing mode for each instruction to print out the proper encoding
    of instruction.
    
    Finally, added addr which, given the extra size of the instruction, fetches that data.
    
    Signed-off-by: pjm <p.micolet@gmail.com>

--- src/nesclo/cpu.clj
+++ src/nesclo/cpu.clj
@@ -1,30 +1,24 @@
 (ns nesclo.cpu)
 
-;;Define registers A,X,Y, Program Counter, Stack Pointer and Status Register
-(def a 0x00)
-(def x 0x00)
-(def y 0x00)
-(def pc 0x00)
-(def s 0x00)
-(def p 0x00)
+;;Define how large an instruction is beyond the instruction itself
+(def instr-size  [0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0,
+                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0,
+                 0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0,
+                 0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0,
+                 0, 2, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 2, 2, 2, 0, 0, 2, 0, 0, 0, 2, 0, 0,
+                 1, 2, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 2, 2, 2, 0, 0, 2, 0, 0, 2, 2, 2, 0,
+                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0,
+                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0,
+                 1, 2, 0, 0, 0, 2, 2, 0, 0, 2, 0, 0, 0, 2, 2, 0])
 
-(def instr-size  [1, 2, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0,
-                 0, 2, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 0, 2, 0, 0,
-                 1, 2, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 2, 2, 2, 0, 1, 2, 1, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
-                 1, 2, 0, 0, 0, 2, 2, 0, 1, 2, 0, 0, 0, 2, 2, 0])
-
+;;Define name of each instruction
 (def instr ["brk","ora","kil","slo","nop","ora","asl","slo","php","ora","asl","anc","nop","ora","asl","slo",
          "bpl","ora","kil","slo","nop","ora","asl","slo","clc","ora","nop","slo","nop","ora","asl","slo",
          "jsr","and","kil","rla","bit","and","rol","rla","plp","and","rol","anc","bit","and","rol","rla",
@@ -42,14 +36,76 @@
          "cpx","sbc","nop","isc","cpx","sbc","inc","isc","inx","sbc","nop","sbc","cpx","sbc","inc","isc",
          "beq","sbc","kil","isc","nop","sbc","inc","isc","sed","sbc","nop","isc","nop","sbc","inc","isc"])
 
+;;Define addressing type of each instruction
+;; 0 implied
+;; 1 immediate
+;; 2 zero page
+;; 3 zero page, X
+;; 4 zero page, Y
+;; 5 indirect index, X
+;; 6 indirect index, Y
+;; 7 absolute
+;; 8 absolute, X
+;; 9 absolute, Y
+;; 10 indirect
+;; 11 relative
+(def type-addr [0,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           7,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           0,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           0,5,0,5,2,2,2,2,0,1,0,1,10,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           1,5,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           1,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           1,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           1,6,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
+           1,6,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
+           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8])
+
 (defn hello []
   (println "hey"))
+
+(defn addr [rom pc size incr]
+  (if (== size 0) 0
+  (if (= size incr)
+    (nth rom (+ pc incr))
+    (+(bit-shift-left (nth rom (+ pc incr)) 2) (addr rom pc size (inc incr))))))
 
 (defn disassemble [rom pc]
   (def inst (get instr (nth rom pc "No more PC") "Last Instruction"))
   (when (not= inst "Last Instruction")
     (do
+        (def addressing (get type-addr (nth rom pc "No pc") "Nil"))
         (def size (get instr-size (nth rom pc)))
-        (println (format "0x%x => %s " pc inst))
-        (recur rom (+ size (inc pc))))
-    ))
+        (if (= addressing 0) 
+          (println (format "0x%05x => %s " pc inst)))
+        (if (= addressing 1) 
+          (println (format "0x%05x => %s #$%02x" pc inst (addr rom pc size 1))))
+        (if (= addressing 2)
+          (println (format "0x%05x => %s $%02x" pc inst (addr rom pc size 1))))
+        (if (= addressing 3)
+          (println (format "0x%05x => %s $%02x,X" pc inst (addr rom pc size 1))))
+        (if (= addressing 4)
+          (println (format "0x%05x => %s $%02x,Y" pc inst (addr rom pc size 1))))
+        (if (= addressing 5) 
+          (println (format "0x%05x => %s ($%02x,X)" pc inst (addr rom pc size 1))))
+        (if (= addressing 6)
+          (println (format "0x%05x => %s ($%02x,Y)" pc inst (addr rom pc size 1))))
+        (if (= addressing 7)
+          (println (format "0x%05x => %s $%04x" pc inst (addr rom pc size 1))))
+        (if (= addressing 8)
+          (println (format "0x%05x => %s $%04x,X" pc inst (addr rom pc size 1))))
+        (if (= addressing 9)
+          (println (format "0x%05x => %s $%04x,Y" pc inst (addr rom pc size 1))))
+        (if (= addressing 10)
+          (println (format "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1))))
+        (if (= addressing 11)
+          (println (format "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1))))
+        (recur rom (+ size (inc pc))))))
+
