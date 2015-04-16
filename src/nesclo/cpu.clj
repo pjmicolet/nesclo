b14d2e3865cdcc6c3c473ff8a7e99c3ac4497cb8
pjm <p.micolet@gmail.com>
2015-04-16 15:51:18 +0100

    Massive Refactoring
    Just trying to stick to the clojure programming guidelines to make it nicer.
    
    Signed-off-by: pjm <p.micolet@gmail.com>

--- src/nesclo/cpu.clj
+++ src/nesclo/cpu.clj
@@ -1,40 +1,40 @@
 (ns nesclo.cpu)
 
 ;;Define how large an instruction is beyond the instruction itself
-(def instr-size  [0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0,
-                 0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0,
-                 0, 2, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0,
-                 0, 2, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 0, 2, 0, 0, 0, 2, 0, 0,
-                 1, 2, 1, 0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 2, 1, 1, 0, 0, 2, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0,
-                 1, 2, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 2, 2, 2, 0,
-                 1, 2, 0, 0, 0, 1, 1, 0, 0, 2, 0, 0, 0, 2, 2, 0])
+(def instr-size  [0 2 0 0 0 1 1 0 0 1 0 0 0 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
+                 1 2 0 0 1 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
+                 0 2 0 0 0 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
+                 0 2 0 0 0 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
+                 0 2 0 0 1 1 1 0 0 0 0 0 2 2 2 0
+                 1 2 0 0 1 1 1 0 0 2 0 0 0 2 0 0
+                 1 2 1 0 1 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 2 1 1 0 0 2 0 0 2 2 2 0
+                 1 2 0 0 1 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
+                 1 2 0 0 1 1 1 0 0 1 0 0 2 2 2 0
+                 1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0])
 
 ;;Define name of each instruction
-(def instr ["brk","ora","kil","slo","nop","ora","asl","slo","php","ora","asl","anc","nop","ora","asl","slo",
-         "bpl","ora","kil","slo","nop","ora","asl","slo","clc","ora","nop","slo","nop","ora","asl","slo",
-         "jsr","and","kil","rla","bit","and","rol","rla","plp","and","rol","anc","bit","and","rol","rla",
-         "bmi","and","kil","rla","nop","and","rol","rla","sec","and","nop","rla","nop","and","rol","rla",
-         "rti","eor","kil","sre","nop","eor","lsr","sre","pha","eor","lsr","alr","jmp","eor","lsr","sre",
-         "bvc","eor","kil","sre","nop","eor","lsr","sre","cli","eor","nop","sre","nop","eor","lsr","sre",
-         "rts","adc","kil","rra","nop","adc","ror","rra","pla","adc","ror","arr","jmp","adc","ror","rra",
-         "bvs","adc","kil","rra","nop","adc","ror","rra","sei","adc","nop","rra","nop","adc","ror","rra",
-         "nop","sta","nop","sax","sty","sta","stx","sax","dey","nop","txa","xaa","sty","sta","stx","sax",
-         "bbc","sta","kil","ahx","sty","sta","stx","sax","tya","sta","txs","tas","shy","sta","shx","ahx",
-         "ldy","lda","ldx","lax","ldy","lda","ldx","lax","tay","lda","tax","lax","ldy","lda","ldx","lax",
-         "bcs","lda","kil","lax","ldy","lda","ldx","lax","clv","lda","tsx","las","ldy","lda","ldx","lax",
-         "cpy","cmp","nop","dcp","cpy","cmp","dec","dcp","iny","cmp","dex","axs","cpy","cmp","dec","dcp",
-         "bne","cmp","kil","dcp","nop","cmp","dec","dcp","cld","cmp","nop","dcp","nop","cmp","dec","dcp",
-         "cpx","sbc","nop","isc","cpx","sbc","inc","isc","inx","sbc","nop","sbc","cpx","sbc","inc","isc",
-         "beq","sbc","kil","isc","nop","sbc","inc","isc","sed","sbc","nop","isc","nop","sbc","inc","isc"])
+(def instr ["brk" "ora" "kil" "slo" "nop" "ora" "asl" "slo" "php" "ora" "asl" "anc" "nop" "ora" "asl" "slo"
+         "bpl" "ora" "kil" "slo" "nop" "ora" "asl" "slo" "clc" "ora" "nop" "slo" "nop" "ora" "asl" "slo"
+         "jsr" "and" "kil" "rla" "bit" "and" "rol" "rla" "plp" "and" "rol" "anc" "bit" "and" "rol" "rla"
+         "bmi" "and" "kil" "rla" "nop" "and" "rol" "rla" "sec" "and" "nop" "rla" "nop" "and" "rol" "rla"
+         "rti" "eor" "kil" "sre" "nop" "eor" "lsr" "sre" "pha" "eor" "lsr" "alr" "jmp" "eor" "lsr" "sre"
+         "bvc" "eor" "kil" "sre" "nop" "eor" "lsr" "sre" "cli" "eor" "nop" "sre" "nop" "eor" "lsr" "sre"
+         "rts" "adc" "kil" "rra" "nop" "adc" "ror" "rra" "pla" "adc" "ror" "arr" "jmp" "adc" "ror" "rra"
+         "bvs" "adc" "kil" "rra" "nop" "adc" "ror" "rra" "sei" "adc" "nop" "rra" "nop" "adc" "ror" "rra"
+         "nop" "sta" "nop" "sax" "sty" "sta" "stx" "sax" "dey" "nop" "txa" "xaa" "sty" "sta" "stx" "sax"
+         "bbc" "sta" "kil" "ahx" "sty" "sta" "stx" "sax" "tya" "sta" "txs" "tas" "shy" "sta" "shx" "ahx"
+         "ldy" "lda" "ldx" "lax" "ldy" "lda" "ldx" "lax" "tay" "lda" "tax" "lax" "ldy" "lda" "ldx" "lax"
+         "bcs" "lda" "kil" "lax" "ldy" "lda" "ldx" "lax" "clv" "lda" "tsx" "las" "ldy" "lda" "ldx" "lax"
+         "cpy" "cmp" "nop" "dcp" "cpy" "cmp" "dec" "dcp" "iny" "cmp" "dex" "axs" "cpy" "cmp" "dec" "dcp"
+         "bne" "cmp" "kil" "dcp" "nop" "cmp" "dec" "dcp" "cld" "cmp" "nop" "dcp" "nop" "cmp" "dec" "dcp"
+         "cpx" "sbc" "nop" "isc" "cpx" "sbc" "inc" "isc" "inx" "sbc" "nop" "sbc" "cpx" "sbc" "inc" "isc"
+         "beq" "sbc" "kil" "isc" "nop" "sbc" "inc" "isc" "sed" "sbc" "nop" "isc" "nop" "sbc" "inc" "isc"])
 
 ;;Define addressing type of each instruction
 ;; 0 implied
@@ -49,27 +49,24 @@
 ;; 9 absolute, Y
 ;; 10 indirect
 ;; 11 relative
-(def type-addr [0,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           7,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           0,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           0,5,0,5,2,2,2,2,0,1,0,1,10,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           1,5,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           1,5,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           1,5,0,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           1,6,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8,
-           1,6,1,5,2,2,2,2,0,1,0,1,7,7,7,7,
-           11,6,0,6,3,3,3,3,0,9,0,9,8,8,8,8])
-
-(defn hello []
-  (println "hey"))
+(def type-addr [0 5 0 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           7 5 0 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           0 5 0 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           0 5 0 5 2 2 2 2 0 1 0 1 10 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           1 5 1 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           1 5 1 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           1 5 0 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           1 6 1 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8 
+           1 6 1 5 2 2 2 2 0 1 0 1 7 7 7 7 
+           11 6 0 6 3 3 3 3 0 9 0 9 8 8 8 8])
 
 (defn addr [rom pc size incr]
   (if (== size 0) 0
@@ -78,35 +75,23 @@
     (+(bit-shift-left (nth rom (+ pc incr)) 2) (addr rom pc size (inc incr))))))
 
 (defn disassemble [rom pc]
-  (def inst (get instr (nth rom pc "No more PC") "Last Instruction"))
+  (let [ inst (get instr (nth rom pc "No more PC") "Last Instruction") ]
   (println pc)
   (when (not= inst "Last Instruction")
-    (do
-        (def addressing (get type-addr (nth rom pc "No pc") "Nil"))
-        (def size (get instr-size (nth rom pc)))
-        (if (= addressing 0) 
-          (println (format "0x%05x => %s " pc inst)))
-        (if (= addressing 1) 
-          (println (format "0x%05x => %s #$%02x" pc inst (addr rom pc size 1))))
-        (if (= addressing 2)
-          (println (format "0x%05x => %s $%02x" pc inst (addr rom pc size 1))))
-        (if (= addressing 3)
-          (println (format "0x%05x => %s $%02x,X" pc inst (addr rom pc size 1))))
-        (if (= addressing 4)
-          (println (format "0x%05x => %s $%02x,Y" pc inst (addr rom pc size 1))))
-        (if (= addressing 5) 
-          (println (format "0x%05x => %s ($%02x,X)" pc inst (addr rom pc size 1))))
-        (if (= addressing 6)
-          (println (format "0x%05x => %s ($%02x,Y)" pc inst (addr rom pc size 1))))
-        (if (= addressing 7)
-          (println (format "0x%05x => %s $%04x" pc inst (addr rom pc size 1))))
-        (if (= addressing 8)
-          (println (format "0x%05x => %s $%04x,X" pc inst (addr rom pc size 1))))
-        (if (= addressing 9)
-          (println (format "0x%05x => %s $%04x,Y" pc inst (addr rom pc size 1))))
-        (if (= addressing 10)
-          (println (format "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1))))
-        (if (= addressing 11)
-          (println (format "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1))))
-        (recur rom (+ size (inc pc))))))
+    (let [ addressing (get type-addr (nth rom pc "No pc") "Nil")
+          size (get instr-size (nth rom pc))]
+    (condp = addressing
+         0 (printf "0x%05x => %s" pc inst)
+         1 (printf "0x%05x => %s #$%02x" pc inst (addr rom pc size 1))
+         2 (printf "0x%05x => %s $%02x" pc inst (addr rom pc size 1))
+         3 (printf "0x%05x => %s $%02x,X" pc inst (addr rom pc size 1))
+         4 (printf "0x%05x => %s $%02x,Y" pc inst (addr rom pc size 1))
+         5 (printf "0x%05x => %s ($%02x,X)" pc inst (addr rom pc size 1))
+         6 (printf "0x%05x => %s ($%02x,Y)" pc inst (addr rom pc size 1))
+         7 (printf "0x%05x => %s $%04x" pc inst (addr rom pc size 1))
+         8 (printf "0x%05x => %s $%04x,X" pc inst (addr rom pc size 1))
+         9 (printf "0x%05x => %s $%04x,Y" pc inst (addr rom pc size 1))
+         10 (printf "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1))
+         11 (printf "0x%05x => %s ($%04x)" pc inst (addr rom pc size 1)))
+    (recur rom (+ size (inc pc)))))))
 
