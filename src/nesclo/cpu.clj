a11af7940de8ef54edc2836ccca5d2ec5c475ec7
pjm <p.micolet@gmail.com>
2015-05-04 15:05:39 +0100

    Added single execution of disassembler.
    And also updated disassemble because it used old type-addr symbol
    
    Signed-off-by: pjm <p.micolet@gmail.com>

--- src/nesclo/cpu.clj
+++ src/nesclo/cpu.clj
@@ -103,9 +103,29 @@
          ~@code)
        (swap! instr-ops assoc ~opcode ~name)))
 
+(defn dis-once [rom pc]
+  (let [ inst (get instr (nth rom pc "No more PC") "Last Instruction") ]
+  (when (not= inst "Last Instruction")
+    (let [ addressing (get type-addr-diss (nth rom pc "No pc") "Nil")
+          size (get instr-size (nth rom pc))]
+    (condp = addressing
+         0 (printf "0x%05x => %s\n" pc inst)
+         1 (printf "0x%05x => %s #$%02x\n" pc inst (addr rom pc size))
+         2 (printf "0x%05x => %s $%02x\n" pc inst (addr rom pc size))
+         3 (printf "0x%05x => %s $%02x,X\n" pc inst (addr rom pc size))
+         4 (printf "0x%05x => %s $%02x,Y\n" pc inst (addr rom pc size))
+         5 (printf "0x%05x => %s ($%02x,X)\n" pc inst (addr rom pc size))
+         6 (printf "0x%05x => %s ($%02x,Y)\n" pc inst (addr rom pc size))
+         7 (printf "0x%05x => %s $%04x\n" pc inst (addr rom pc size))
+         8 (printf "0x%05x => %s $%04x,X\n" pc inst (addr rom pc size))
+         9 (printf "0x%05x => %s $%04x,Y\n" pc inst (addr rom pc size))
+         10 (printf "0x%05x => %s ($%04x)\n" pc inst (addr rom pc size))
+         11 (printf "0x%05x => %s ($%04x)\n" pc inst (addr rom pc size)))))))
+
 (defn disassemble [rom pc]
   (let [ inst (get instr (nth rom pc "No more PC") "Last Instruction") ]
   (when (not= inst "Last Instruction")
+    (let [ addressing (get type-addr-diss (nth rom pc "No pc") "Nil")
     (let [ addressing (get type-addr (nth rom pc "No pc") "Nil")
           size (get instr-size (nth rom pc))]
     (condp = addressing
