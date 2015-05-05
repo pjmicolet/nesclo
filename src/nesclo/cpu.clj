75ba8f0c4151687f0e301cd300c20f7f51159346
pjm <p.micolet@gmail.com>
2015-05-05 23:37:48 +0100

    Fixed size for an instruction.
    jsr-abs is pc + 2 bytes not + 1 byte.
    
    Signed-off-by: pjm <p.micolet@gmail.com>

--- src/nesclo/cpu.clj
+++ src/nesclo/cpu.clj
@@ -7,7 +7,7 @@
 ;;Define how large an instruction is beyond the instruction itself
 (def instr-size  [0 2 0 0 0 1 1 0 0 1 0 0 0 2 2 0
                  1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
-                 1 2 0 0 1 1 1 0 0 1 0 0 2 2 2 0
+                 2 2 0 0 1 1 1 0 0 1 0 0 2 2 2 0
                  1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
                  0 2 0 0 0 1 1 0 0 1 0 0 2 2 2 0
                  1 2 0 0 0 1 1 0 0 2 0 0 0 2 2 0
